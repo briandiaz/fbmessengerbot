5e89ce5a3001239b98f692886715e72faf6afabd
Brian Diaz <briandiaz@outlook.com>
2016-05-02 05:31:27 -0400

    Add forecast response.

--- src/lib/facebook_messenger.js
+++ src/lib/facebook_messenger.js
@@ -5,6 +5,8 @@ import libdebug from 'debug';
 dotenv.load();
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const FACEBOOK_MESSAGES_URL = 'https://graph.facebook.com/v2.6/me/messages';
+const WEATHER_APP_ID = process.env.WEATHER_APP_ID;
+const WEATHER_API_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=' + WEATHER_APP_ID;
 const debug = libdebug('fbmessenger:facebookmessenger');
 
 /**
@@ -20,8 +22,8 @@ function FacebookMessenger() {
   * @param {Number} senderId - Required. Sender Id.
   * @param {String} text - Required. Message to send.
   */
-  function * sendSingleMessage(senderId, text) {
-    const messageData = {
+  function * sendMessage(senderId, text, messageData = null) {
+    const message = messageData || {
       text: text,
     };
     const body = {
@@ -29,7 +31,7 @@ function FacebookMessenger() {
         recipient: {
           id: senderId,
         },
-        message: messageData,
+        message: message,
       },
       method: 'POST',
       qs: {
@@ -45,8 +47,37 @@ function FacebookMessenger() {
   * @param {String} text - Required. Message to send.
   */
   this.sendTextMessage = function * sendTextMessage(senderId, text) {
-    return yield sendSingleMessage(senderId, text);
+    return yield sendMessage(senderId, text);
   };
+
+  function * sendForecast(senderId, message) {
+    const city = message.split('weather')[1].replace('of', '').trim();
+    const url = WEATHER_API_URL + '&q=' + city;
+    const forecast = yield request(url, {
+      method: 'GET',
+    });
+    if (forecast.name === undefined) {
+      return yield sendMessage(senderId, 'City not found!');
+    }
+    console.log(forecast);
+    // Convert Kelvin to Celsius
+    const temperature = (forecast.main.temp - 273.15);
+    const messageData = {
+      'attachment': {
+        'type': 'template',
+        'payload': {
+          'template_type': 'generic',
+          'elements': [{
+            'title': temperature + ' °C',
+            'subtitle': forecast.name,
+            'image_url': 'http://openweathermap.org/img/w/' + forecast.weather[0].icon + '.png',
+          },
+          ],
+        },
+      },
+    };
+    return yield sendMessage(senderId, '', messageData);
+  }
 
   this.processEvents = function * processEvents(events) {
     for (let i = 0; i < events.length; i++) {
@@ -55,7 +86,10 @@ function FacebookMessenger() {
       const message = (event.message && event.message.text) ? event.message.text : undefined;
       if (message) {
         console.log(`Sender: ${sender} Message: ${message}`);
-        yield sendSingleMessage(sender, 'Hello from the cloud :)');
+        if (message.includes('weather')) {
+          yield sendForecast(sender, message);
+        }
+        yield sendMessage(sender, 'Hello from the cloud :)');
         debug(`Sender: ${sender}`);
         debug(`Message: ${message}`);
       }
